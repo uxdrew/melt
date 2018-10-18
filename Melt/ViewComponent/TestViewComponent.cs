8fc045721291fafd6a30a531273ca8f52f172459
Arjun Balaji <arjun.r.balaji@gmail.com>
2018-10-18 07:49:01 -0600

    Added sample TestDb Razor page and sample code inside there. Added a ViewComponent.

---
+++ Melt/ViewComponent/TestViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Melt.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Melt.ViewComponent
+{
+    public class TestViewComponent : Microsoft.AspNetCore.Mvc.ViewComponent
+    {
+        private ISqlAccess _sqlAccess;
+
+        public string DateFirst { get; set; }
+
+        public TestViewComponent(ISqlAccess sqlAcccess)
+        {
+            _sqlAccess = sqlAcccess;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var date = _sqlAccess.ExecuteScalarQuery<DateTime>("select GetDate()");
+            return View("Default", date.ToLongDateString());
+        }
+    }
+}
