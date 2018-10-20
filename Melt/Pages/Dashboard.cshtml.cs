3125f4d23f8c98cc2d62e1065930e2f7f6ed3cf5
Arjun Balaji <arjun.r.balaji@gmail.com>
2018-10-20 15:48:07 -0600

    Check in bad climber image.

--- Melt/Pages/Dashboard.cshtml.cs
+++ Melt/Pages/Dashboard.cshtml.cs
@@ -1,16 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Melt.Pages
 {
     public class DashboardModel : PageModel
     {
+        public int ClimberTop { get; set; }
+        public int ClimberBottom { get; set; }
         public void OnGet()
         {
         }
+
+        //private int ComputeClimberoffset()
+        //{
+
+        //}
     }
 }
