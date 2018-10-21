f8a7a364e0077cec487c630085d4e01b31dae7b7
Arjun Balaji <arjun.r.balaji@gmail.com>
2018-10-20 18:34:39 -0600

    More of the same check in

--- Melt/Pages/Dashboard.cshtml.cs
+++ Melt/Pages/Dashboard.cshtml.cs
@@ -5,9 +5,15 @@ namespace Melt.Pages
     public class DashboardModel : PageModel
     {
         public decimal PercentToWorthlessness { get; set; }
+        public string ClimberPath { get; set; }
         public decimal Offset { get; set; }
         public void OnGet()
         {
+            ClimberPath = "~/images/climber.png";
+            if(PercentToWorthlessness == 0)
+            {
+                ClimberPath = "~/images/winningClimber.png";
+            }
             Offset = ((PercentToWorthlessness * 3.5M) / 100) + 1;
 
         }
